80219e375918f01395be7927710582b2e53425a5
amine triki <amine.triki15@gmail.com>
2023-08-15 23:44:35 +0100

    start settings box

--- js/main.js
+++ js/main.js
@@ -6,6 +6,16 @@ setInterval(() => {
 time.innerHTML = date.toLocaleTimeString();
 }, 1000);
 
+//Toggle spin class on icon
+document.querySelector(".toggle-settings .fa-gear").onclick = function() {
+    
+    // toggle class fa-spin for rotation on self 
+    this.classList.toggle("fa-spin");
+
+    // toggle class open on main settings box 
+    document.querySelector(".settings-box").classList.toggle("open");
+    
+};
 
 
 
