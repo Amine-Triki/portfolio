6609960c5a4d9015c51238ade4283695bd0f71e1
amine triki <amine.triki15@gmail.com>
2023-08-26 00:51:42 +0100

    change second color

--- js/main.js
+++ js/main.js
@@ -12,6 +12,22 @@ let mainColors = localStorage.getItem("color_option");
 if (mainColors !== null ) {
     document.documentElement.style.setProperty('--main-color' , mainColors);
 
+
+  // change second color
+  if ( mainColors === "#a8c9fa") {
+    document.documentElement.style.setProperty('--second-color' , "#C79471");
+} else if ( mainColors === "#427624") {
+    document.documentElement.style.setProperty('--second-color' , "#9CDBAD");
+} else if (mainColors  === "#762473") {
+    document.documentElement.style.setProperty('--second-color' , "#A8A723");
+}else if (mainColors  === "#847625") {
+    document.documentElement.style.setProperty('--second-color' , "#2164B8");
+} else {
+    document.documentElement.style.setProperty('--second-color' , "#93B0EF");
+}
+
+
+
      // remove active class from all colors list item
     document.querySelectorAll(".colors-list li").forEach(element => {
 
@@ -62,24 +78,28 @@ colorsli.forEach(li => {
 
         // add active class on self 
         e.target.classList.add("active");
+    
 
-
-        /* // change second color
-        if (e.target.dataset.color === "#a8c9fa") {
-            document.documentElement.style.setProperty('--second-color' , "#f00");
-        } else if (e.target.dataset.color === "#427624") {
-            document.documentElement.style.setProperty('--second-color' , "#0ff");
-        } else if (e.target.dataset.color === "#762473") {
-            document.documentElement.style.setProperty('--second-color' , "#ff0");
-        }else if (e.target.dataset.color === "#847625") {
-            document.documentElement.style.setProperty('--second-color' , "#f0f");
-        } else {
-            document.documentElement.style.setProperty('--second-color' , "#93B0EF");
-        }
-
-         */
+            // change second color
+                if ( e.target.dataset.color === "#a8c9fa") {
+                    document.documentElement.style.setProperty('--second-color' , "#C79471");
+                } else if ( e.target.dataset.color === "#427624") {
+                    document.documentElement.style.setProperty('--second-color' , "#9CDBAD");
+                } else if (e.target.dataset.color  === "#762473") {
+                    document.documentElement.style.setProperty('--second-color' , "#A8A723");
+                }else if (e.target.dataset.color  === "#847625") {
+                    document.documentElement.style.setProperty('--second-color' , "#2164B8");
+                } else {
+                    document.documentElement.style.setProperty('--second-color' , "#93B0EF");
+                }
+     
+        
     });
 });
+
+
+
+
 
 
 
