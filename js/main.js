5f8745d971c56269ba6619baf58960d3b43d18dc
amine triki <amine.triki15@gmail.com>
2023-08-26 22:44:31 +0100

    end website portfolio

--- js/main.js
+++ js/main.js
@@ -98,6 +98,18 @@ colorsli.forEach(li => {
 });
 
 
+//skills
+let section = document.querySelector(".Skill");
+let spans = document.querySelectorAll(".progress span");
+
+window.onscroll = function () {
+  if (window.scrollY >= section.offsetTop - 300) {
+    spans.forEach((span) => {
+      span.style.width = span.dataset.width;
+    });
+  }
+};
+
 
 
 
